7c5fd0fe232a683b9846265ceffe674bacf47b56
John Cameron <john.n.cameron@gmail.com>
2019-08-24 23:08:40 +1000

    initial

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,97 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="absolute-center">All My Notes</q-toolbar-title>
+
+        <div>Write some stuff...</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer>
+      <q-tabs>
+        <q-route-tab
+          v-for="nav in navs"
+          :key="nav.label"
+          :icon="nav.icon"
+          :label="nav.label"
+          :to="nav.to"
+        />
+      </q-tabs>
+    </q-footer>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      :breakpoint="767"
+      :width="250"
+      bordered
+      content-class="bg-primary"
+    >
+      <q-list dark>
+        <q-item-label header>Navigation</q-item-label>
+        <q-item
+          class="text-grey-4"
+          v-for="nav in navs"
+          :key="nav.label"
+          :to="nav.to"
+          exact
+          clickable
+        >
+          <q-item-section avatar>
+            <q-icon :name="nav.icon" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{nav.label}}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from "quasar";
+
+export default {
+  name: "MyLayout",
+  data() {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      navs: [
+        {
+          label: "Todo",
+          icon: "list",
+          to: "/"
+        },
+        {
+          label: "Settings",
+          icon: "settings",
+          to: "/settings"
+        }
+      ]
+    };
+  },
+  methods: {
+    openURL
+  }
+};
+</script>
+
+<style lang="scss">
+@media screen and (min-width: 768px) {
+  .q-footer {
+    display: none;
+  }
+}
+
+.q-drawer {
+  .q-router-link--exact-active {
+    color: #ffbb00 !important;
+    font-weight: 500;
+  }
+}
+</style>
